0fa513acf31a9b776e9224d97dd3de3168b647cf
RasyidJu <abdulrasyid13uu@gmail.com>
2024-06-06 09:49:10 +0700

    update

--- strukturorganisasi.php
+++ strukturorganisasi.php
@@ -5,7 +5,6 @@ $isLoggedIn = isset($_SESSION['username']);
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

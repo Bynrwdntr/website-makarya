c9d1c1a77ce5cbf48433d99c5d17ce6d0132fe33
RasyidJu <abdulrasyid13uu@gmail.com>
2024-06-08 10:53:01 +0700

    update

--- fasilitas.php
+++ fasilitas.php
@@ -261,60 +261,71 @@ $isLoggedIn = isset($_SESSION['username']);
     </main>
 
    <!-- Footer Section -->
-   <footer class="bg-gray-800 text-gray-300 mt-8">
-        <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row justify-between">
-            <div class="w-full sm:w-auto sm:px-4 mb-4 sm:mb-0">
-                <h2 class="title-font font-medium text-gray-400 tracking-widest text-sm mb-3">HUBUNGI KAMI:</h2>
-                <nav class="list-none mb-10">
-                    <li>
-                        <a href="https://wa.me/1234567890" class="text-gray-400 hover:text-gray-200">
-                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M12 2C6.477 2 2 6.477 2 12c0 1.919.542 3.705 1.478 5.241L2 22l4.887-1.269A9.953 9.953 0 0012 22c5.523 0 10-4.477 10-10S17.523 2 12 2zm0 18a8.947 8.947 0 01-4.753-1.361L7 18l-.264.077A7.973 7.973 0 014 12c0-4.418 3.582-8 8-8s8 3.582 8 8-3.582 8-8 8zm3.97-5.273l-1.204-.551a1.122 1.122 0 00-1.126.095 4.99 4.99 0 01-1.676.659c-1.238.256-2.297-.121-3.182-1.009-1.56-1.558-2.289-3.145-2.187-4.757a4.95 4.95 0 01.704-2.4 1.118 1.118 0 00-.28-1.408L7.27 7.28a1.121 1.121 0 00-1.271-.08 6.345 6.345 0 00-1.723 1.28A7.947 7.947 0 002 12c0 4.418 3.582 8 8 8a7.95 7.95 0 004.632-1.482 6.342 6.342 0 001.28-1.723 1.119 1.119 0 00-.087-1.27z"></path>
-                            </svg>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="mailto:smkm@makarya.sch.id" class="text-gray-400 hover:text-gray-200">smkm@makarya.sch.id</a>
-                    </li>
-                </nav>
-            </div>
-            <div class="w-full sm:w-auto sm:px-4 mb-4 sm:mb-0 text-center">
-                <h2 class="title-font font-medium text-gray-400 tracking-widest text-sm mb-3">IKUTI KAMI:</h2>
-                <nav class="list-none mb-10 flex justify-center space-x-4">
-                    <li>
+   <footer class="bg-gray-900 text-gray-300 mt-8">
+    <div class="container mx-auto py-6 px-5 flex flex-wrap sm:flex-nowrap justify-between items-start">
+        <div class="w-full sm:w-1/3 mb-6 sm:mb-0">
+            <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">HUBUNGI KAMI</h2>
+            <nav class="list-none">
+                <li class="mb-2">
+                    <a href="https://wa.me/+6281311121465" class="flex items-center text-gray-400 hover:text-gray-200">
+                        <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M12 2C6.477 2 2 6.477 2 12c0 1.919.542 3.705 1.478 5.241L2 22l4.887-1.269A9.953 9.953 0 0012 22c5.523 0 10-4.477 10-10S17.523 2 12 2zm0 18a8.947 8.947 0 01-4.753-1.361L7 18l-.264.077A7.973 7.973 0 014 12c0-4.418 3.582-8 8-8s8 3.582 8 8-3.582 8-8 8zm3.97-5.273l-1.204-.551a1.122 1.122 0 00-1.126.095 4.99 4.99 0 01-1.676.659c-1.238.256-2.297-.121-3.182-1.009-1.56-1.558-2.289-3.145-2.187-4.757a4.95 4.95 0 01.704-2.4 1.118 1.118 0 00-.28-1.408L7.27 7.28a1.121 1.121 0 00-1.271-.08 6.345 6.345 0 00-1.723 1.28A7.947 7.947 0 002 12c0 4.418 3.582 8 8 8a7.95 7.95 0 004.632-1.482 6.342 6.342 0 001.28-1.723 1.119 1.119 0 00-.087-1.27z"></path>
+                        </svg>Sri Wahyuni
+                    </a>
+                    <a href="https://wa.me/+6282298743698" class="flex items-center text-gray-400 hover:text-gray-200">
+                        <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M12 2C6.477 2 2 6.477 2 12c0 1.919.542 3.705 1.478 5.241L2 22l4.887-1.269A9.953 9.953 0 0012 22c5.523 0 10-4.477 10-10S17.523 2 12 2zm0 18a8.947 8.947 0 01-4.753-1.361L7 18l-.264.077A7.973 7.973 0 014 12c0-4.418 3.582-8 8-8s8 3.582 8 8-3.582 8-8 8zm3.97-5.273l-1.204-.551a1.122 1.122 0 00-1.126.095 4.99 4.99 0 01-1.676.659c-1.238.256-2.297-.121-3.182-1.009-1.56-1.558-2.289-3.145-2.187-4.757a4.95 4.95 0 01.704-2.4 1.118 1.118 0 00-.28-1.408L7.27 7.28a1.121 1.121 0 00-1.271-.08 6.345 6.345 0 00-1.723 1.28A7.947 7.947 0 002 12c0 4.418 3.582 8 8 8a7.95 7.95 0 004.632-1.482 6.342 6.342 0 001.28-1.723 1.119 1.119 0 00-.087-1.27z"></path>
+                        </svg>Banu Cahyo
+                    </a>
+                    <a href="https://wa.me/+6285779019823" class="flex items-center text-gray-400 hover:text-gray-200">
+                        <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M12 2C6.477 2 2 6.477 2 12c0 1.919.542 3.705 1.478 5.241L2 22l4.887-1.269A9.953 9.953 0 0012 22c5.523 0 10-4.477 10-10S17.523 2 12 2zm0 18a8.947 8.947 0 01-4.753-1.361L7 18l-.264.077A7.973 7.973 0 014 12c0-4.418 3.582-8 8-8s8 3.582 8 8-3.582 8-8 8zm3.97-5.273l-1.204-.551a1.122 1.122 0 00-1.126.095 4.99 4.99 0 01-1.676.659c-1.238.256-2.297-.121-3.182-1.009-1.56-1.558-2.289-3.145-2.187-4.757a4.95 4.95 0 01.704-2.4 1.118 1.118 0 00-.28-1.408L7.27 7.28a1.121 1.121 0 00-1.271-.08 6.345 6.345 0 00-1.723 1.28A7.947 7.947 0 002 12c0 4.418 3.582 8 8 8a7.95 7.95 0 004.632-1.482 6.342 6.342 0 001.28-1.723 1.119 1.119 0 00-.087-1.27z"></path>
+                        </svg>Defri Prasetya
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:smk.makaryalarangan@gmail.com" class="text-gray-400 hover:text-gray-200">smk.makaryalarangan@gmail.com</a>
+                </li>
+            </nav>
+        </div>
+        <div class="w-full sm:w-1/3 mb-6 sm:mb-0 text-center">
+            <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">IKUTI KAMI</h2>
+            <nav class="list-none flex justify-center space-x-4">
+                <li>
                     <a href="https://www.instagram.com/smk.makaryalarangan/" class="text-gray-400 hover:text-gray-200">
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="M19.615 3.184c-2.108-1.298-5.527-1.531-7.568-1.531s-5.462.234-7.568 1.531c-2.13 1.304-3.147 3.437-3.147 6.416v4.801c0 2.98 1.017 5.112 3.147 6.416 2.108 1.298 5.527 1.531 7.568 1.531s5.462-.234 7.568-1.531c2.13-1.304 3.147-3.437 3.147-6.416v-4.801c0-2.98-1.017-5.112-3.147-6.416zm-7.615 12.919c-3.151 0-5.682-2.531-5.682-5.682s2.531-5.682 5.682-5.682 5.682 2.531 5.682 5.682-2.531 5.682-5.682 5.682zm6.448-10.573c-.737 0-1.335-.598-1.335-1.335s.598-1.335 1.335-1.335 1.335.598 1.335 1.335-.598 1.335-1.335 1.335zm-6.448 1.946c-2.025 0-3.674 1.649-3.674 3.674s1.649 3.674 3.674 3.674 3.674-1.649 3.674-3.674-1.649-3.674-3.674-3.674z"/>
-                    </svg>
-                </a>
-                <a href="https://www.youtube.com/@smkmakaryatangerang4734" class="text-gray-400 hover:text-gray-200">
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="M22.54 6.42c-.2-1.42-.85-2.72-2.26-3.33-2.05-.91-10.26-.91-10.26-.91s-8.21 0-10.26.91c-1.41.61-2.06 1.91-2.26 3.33-.23 1.63-.23 5.03-.23 5.03s0 3.39.23 5.03c.2 1.42.85 2.72 2.26 3.33 2.05.91 10.26.91 10.26.91s8.21 0 10.26-.91c1.41-.61 2.06-1.91 2.26-3.33.23-1.63.23-5.03.23-5.03s0-3.39-.23-5.03zm-13.95 8.99v-7.86l6.15 3.93-6.15 3.93z"/>
-                    </svg>
-                </a>
-                    </li>
-                </nav>
-            </div>
-            <div class="w-full sm:w-auto sm:px-4 text-right">
-                <h2 class="title-font font-medium text-gray-400 tracking-widest text-sm mb-3">ALAMAT:</h2>
-                <nav class="list-none mb-10">
-                    <li>
-                        <span class="text-gray-400">Kp Blok Wareng No.58, RT.003/RW.004, Larangan Selatan</span>
-                    </li>
-                    <li>
-                        <span class="text-gray-400">Kecamatan Larangan, Kota Tangerang, Banten</span>
-                    </li>
-                    <li>
-                        <span class="text-gray-400">Kode Pos 15154</span>
-                    </li>
-                </nav>
-            </div>
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M19.615 3.184c-2.108-1.298-5.527-1.531-7.568-1.531s-5.462.234-7.568 1.531c-2.13 1.304-3.147 3.437-3.147 6.416v4.801c0 2.98 1.017 5.112 3.147 6.416 2.108 1.298 5.527 1.531 7.568 1.531s5.462-.234 7.568-1.531c2.13-1.304 3.147-3.437 3.147-6.416v-4.801c0-2.98-1.017-5.112-3.147-6.416zm-7.615 12.919c-3.151 0-5.682-2.531-5.682-5.682s2.531-5.682 5.682-5.682 5.682 2.531 5.682 5.682-2.531 5.682-5.682 5.682zm6.448-10.573c-.737 0-1.335-.598-1.335-1.335s.598-1.335 1.335-1.335 1.335.598 1.335 1.335-.598 1.335-1.335 1.335zm-6.448 1.946c-2.025 0-3.674 1.649-3.674 3.674s1.649 3.674 3.674 3.674 3.674-1.649 3.674-3.674-1.649-3.674-3.674-3.674z"/>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/@smkmakaryatangerang4734" class="text-gray-400 hover:text-gray-200">
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M22.54 6.42c-.2-1.42-.85-2.72-2.26-3.33-2.05-.91-10.26-.91-10.26-.91s-8.21 0-10.26.91c-1.41.61-2.06 1.91-2.26 3.33-.23 1.63-.23 5.03-.23 5.03s0 3.39.23 5.03c.2 1.42.85 2.72 2.26 3.33 2.05.91 10.26.91 10.26.91s8.21 0 10.26-.91c1.41-.61 2.06-1.91 2.26-3.33.23-1.63.23-5.03.23-5.03s0-3.39-.23-5.03zm-13.95 8.99v-7.86l6.15 3.93-6.15 3.93z"/>
+                        </svg>
+                    </a>
+                </li>
+            </nav>
         </div>
-          <div class="mt-4 text-center">
-                <p class="text-gray-300">&copy; 2023 SMK MAKARYA 3 TANGERANG. All rights reserved.</p>
-            </div>
+        <div class="w-full sm:w-1/3 text-right">
+            <h2 class="title-font font-medium text-white tracking-widest text-sm mb-3">ALAMAT</h2>
+            <nav class="list-none">
+                <li class="mb-2">
+                    <span class="text-gray-400">Kp Blok Wareng No.58, RT.003/RW.004, Larangan Selatan</span>
+                </li>
+                <li class="mb-2">
+                    <span class="text-gray-400">Kecamatan Larangan, Kota Tangerang, Banten</span>
+                </li>
+                <li>
+                    <span class="text-gray-400">Kode Pos 15154</span>
+                </li>
+            </nav>
         </div>
-    </footer>
+    </div>
+    <div class="mt-4 text-center">
+        <p class="text-gray-400">&copy; 2023 SMK MAKARYA TANGERANG. All rights reserved.</p>
+    </div>
+</footer>
 
     <script src="https://cdn.jsdelivr.net/npm/flowbite@1.6.0/dist/flowbite.js"></script>
     <script>
